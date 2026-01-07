7d8db44de6baea435761feee1f4930c1e05f8a87
Abid <abid2020@gmail.com>
2026-01-07 16:11:07 +0530

    Update deployment configuration to Cloudflare Pages
    - Changed deployment method from GitHub Pages to Cloudflare Pages in README.md.
    - Updated GitHub Actions workflow to deploy to Cloudflare Pages, including necessary steps for building and deploying the site.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from "astro/config";
-import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://ekuttan.in",
   base: "/",
-  integrations: [tailwind(), sitemap()],
+  integrations: [tailwind()],
 });
